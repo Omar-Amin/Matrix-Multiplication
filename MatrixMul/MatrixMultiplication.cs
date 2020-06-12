80c1b064c6a7e785539d2f917b8298468844c99c
Omar Amin <omar_75@hotmail.dk>
2020-06-12 15:57:31 +0200

    Added a transposed version.

--- MatrixMul/MatrixMultiplication.cs
+++ MatrixMul/MatrixMultiplication.cs
@@ -1,6 +1,7 @@
 using System;
 public class MatrixMultiplication
 {
+    /// <summary>Normal matrix multiplication.</summary>
     public int[,] normal(int[,] a, int[,] b)
     {
         int aRow = a.GetLength(0);
@@ -17,6 +18,41 @@ public class MatrixMultiplication
                 for (int k = 0; k < aCol; k++)
                 {
                     cValue += a[i, k] * b[k, j];
+                }
+                c[i, j] = cValue;
+            }
+        }
+
+        return c;
+    }
+
+    /// <summary>Transposed matrix multiplication.</summary>
+    public int[,] transposed(int[,] a, int[,] b)
+    {
+        int aRow = a.GetLength(0);
+        int aCol = a.GetLength(1);
+        int bCol = b.GetLength(1);
+
+        int[,] newB = new int[b.GetLength(1), b.GetLength(0)];
+
+        for (int i = 0; i < b.GetLength(0); i++)
+        {
+            for (int j = 0; j < bCol; j++)
+            {
+                newB[j, i] = b[i, j];
+            }
+        }
+
+        int[,] c = new int[aRow, bCol];
+
+        for (int i = 0; i < aRow; i++)
+        {
+            for (int j = 0; j < bCol; j++)
+            {
+                int cValue = 0;
+                for (int k = 0; k < aCol; k++)
+                {
+                    cValue += a[i, k] * newB[j, k];
                 }
                 c[i, j] = cValue;
             }
